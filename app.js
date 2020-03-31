49b3c63c6226aebb3de0a629fc3e49d4a580370f
xmagnetisx <muhammad.trad@yahoo.com>
2020-03-31 02:12:34 -0700

    fourth commit

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+
+class Store{
+    constructor(func){
+
+    }
+
+
+}
+
+
+
+
+//Combining Reducers
 const combineReducers = (obj) => {
         return (prevState, action) =>{
           let stateTypes = Object.keys(obj);
@@ -10,7 +23,6 @@ const combineReducers = (obj) => {
             let output = reducerFunc(prevState[stateType], action);
             newObj[stateType] = output;
         }
-        console.log(newObj);
         return newObj;
     }
 };
@@ -55,5 +67,34 @@ myRootReducer(newState, myInconsequentialAction)
 
 
 // The first time you call `myRootReducer` with `myInconsequentialAction`, it returns the initial state. This is because the only reducer, `myNoiseReducer`, doesn't respond to that action type. When we invoke it with `newState` and `myNoisyAction`, however, the `noise` key is modified because `myNoiseReducer` returns something other than its `prevState` for the `"noisy action"` action type. When we invoke the `rootReducer`, with `myInconsequentialAction` the second time, the `noise` property doesn't revert back to it's default value, it just doesn't change.
+////////////////////////////////////////////////////////////////////////////
+//Dispatch
+//const dispatch = 
 
+// define a reducer for user:
+const userReducer = (oldUser = null, action) => {
+  if (action.type === "new user") {
+    return action.user;
+  }
+  return oldUser;
+};
 
+// create a rootReducer:
+const rootReducer = combineReducers({
+  user: userReducer
+});
+
+// create a store using the rootReducer:
+const store = new Store(rootReducer);
+
+// get the state:
+store.getState(); // => {}
+
+// invoke the dispatch function to update the user key:
+const action = {
+  type: "new user",
+  user: "Jeffrey Fiddler"
+};
+
+store.dispatch(action);
+store.getState(); // => { user: "Jeffrey Fiddler" }
